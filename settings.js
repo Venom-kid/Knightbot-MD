f0941d22e5ac290f8b4309ec1853af4d68bc5037
Venom-kid <138268886+Venom-kid@users.noreply.github.com>
2025-04-10 20:50:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Dre',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Andre Bot",
+  botOwner: 'Andre', // Your name
+  ownerNumber: '2348020861112', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  commandMode: "private",
+  description: "This is a bot for managing group commands and automating tasks ordered by DeAndre.",
   version: "1.0.0",
 };
 
